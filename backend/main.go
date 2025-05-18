687e468f8b44b2173023a5cd487328c26bf44ff3
Don Mennerich <don.mennerich@nyu.edu>
2025-05-18 19:43:56 -0400

    adding mariadb

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,86 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	gin "github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func handler(c *gin.Context) {
-	c.JSON(200, "Hello from Gin in Docker!")
-}
+var db *sql.DB
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) { handler(c) })
+	var err error
+	if db, err = connect(); err != nil {
+		panic(err)
+	}
 
+	if err = prepare(); err != nil {
+		panic(err)
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) { blogHandler(c) })
 	fmt.Println("Go backend started!")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func connect() (*sql.DB, error) {
+	bin, err := os.ReadFile("/run/secrets/db-password")
+	if err != nil {
+		return nil, err
+	}
+	return sql.Open("mysql", fmt.Sprintf("root:%s@tcp(db:3306)/example", string(bin)))
+}
+
+func prepare() error {
+
+	for i := 0; i < 60; i++ {
+		if err := db.Ping(); err == nil {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+
+	if _, err := db.Exec("DROP TABLE IF EXISTS blog"); err != nil {
+		return err
+	}
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS blog (id int NOT NULL AUTO_INCREMENT, title varchar(255), PRIMARY KEY (id))"); err != nil {
+		return err
+	}
+
+	for i := 0; i < 5; i++ {
+		if _, err := db.Exec("INSERT INTO blog (title) VALUES (?);", fmt.Sprintf("Blog post #%d", i)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func blogHandler(c *gin.Context) {
+	db, err := connect()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT title FROM blog")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	var titles []string
+	for rows.Next() {
+		var title string
+		err = rows.Scan(&title)
+		titles = append(titles, title)
+	}
+	c.JSON(200, titles)
 }
